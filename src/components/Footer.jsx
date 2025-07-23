80295977c81217c09b1e39f6c23da2d00920640a
Harry Liu <harryliu4154@hotmail.com>
2025-07-23 01:45:28 -0400

    footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 export default function Footer() {
     return (
         <footer>
+            <div className="bgGradient"></div>
             <div>
                 <h2>Martian Landscape</h2>
                 <h1>NASA REACT PROJECT</h1>
