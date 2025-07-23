c0ceab59779bcb6e990dbd3bb159e5d45c76631a
Harry Liu <harryliu4154@hotmail.com>
2025-07-23 00:31:27 -0400

    added placeholder content to sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,11 @@
 export default function SideBar() {
     return (
         <div>
-            
+            <h2>Martian Landscape</h2>
+            <div>
+                <p>Description</p>
+                <p>Placeholder text about topic</p>
+            </div>
         </div>
     )
 }
