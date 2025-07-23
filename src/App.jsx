86060a083a56833bd8074382916455564336de46
Harry Liu <harryliu4154@hotmail.com>
2025-07-23 00:13:40 -0400

    added components to app tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Footer from "./components/footer"
+import Main from "./components/main"
+import SideBar from "./components/SideBar"
+
 function App() {
 
   return (
     <>
+      <SideBar/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
