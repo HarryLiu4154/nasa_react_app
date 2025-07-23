0e7b4c6a4c8ee70804d391e4ebdee44301a6dcc7
Harry Liu <harryliu4154@hotmail.com>
2025-07-23 02:42:24 -0400

    sidebar styling

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,16 @@
 export default function SideBar() {
     return (
-        <div>
-            <h2>Martian Landscape</h2>
-            <div>
-                <p>Description</p>
-                <p>Placeholder text about topic</p>
+        <div className="sidebar">
+            <div className="bgOverlay"></div>
+            <div className="sidebarContent">
+                <h2>Martian Landscape</h2>
+                <div>
+                    <p>Description</p>
+                    <p>Placeholder text about topic</p>
+                </div>
+                <button>
+                    <i className="fa-solid fa-arrow-right"></i>
+                </button>
             </div>
         </div>
     )
