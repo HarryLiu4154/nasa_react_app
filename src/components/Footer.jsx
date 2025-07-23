fcb89ede5b0eecd63462f453d5e51e1af6f3307c
Harry Liu <harryliu4154@hotmail.com>
2025-07-23 00:29:42 -0400

    added content to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 export default function Footer() {
     return (
         <footer>
-            
+            <div>
+                <h2>Martian Landscape</h2>
+                <h1>NASA REACT PROJECT</h1>
+            </div>
+            <button>
+                <i className="fa-solid fa-circle-info"></i>
+            </button>
         </footer>
     )
 }
